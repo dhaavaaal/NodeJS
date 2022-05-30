6c9b3483552a6928872ba1c5ef679666677751d9
Dhaval Thakkar <dhavalthakkar1541@gmail.com>
2022-05-30 18:51:02 +0530

    minor updates. not so relatable

--- Udemy/Express-Module-5/routes/admin.js
+++ Udemy/Express-Module-5/routes/admin.js
@@ -17,7 +17,6 @@ router.get("/add-product", (req, res, next) => {
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
-  console.log(rootDir, process.mainModule);
   res.redirect("/");
 });
 
