2d26f9070729a641dee1c971d8ce9dfe7a936a47
Dhaval Thakkar <dhavalthakkar1541@gmail.com>
2022-05-31 12:23:04 +0530

    Done: Completed with module 6. To continue with MVC now.

--- Udemy/DynamicContent-Module6/routes/shop.js
+++ Udemy/DynamicContent-Module6/routes/shop.js
@@ -8,8 +8,16 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("shop.js", adminData.products);
-  res.render("shop");
+  // console.log("shop.js", adminData.products);
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
