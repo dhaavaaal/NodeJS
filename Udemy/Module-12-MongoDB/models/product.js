d0ef173aa6774a948e915babd513c60e2516e424
Dhaval Thakkar <dhavalthakkar1541@gmail.com>
2023-03-20 11:01:17 +0530

    mongoDB module

---
+++ Udemy/Module-12-MongoDB/models/product.js
@@ -0,0 +1,57 @@
+const getDb = require("../util/database").getDb;
+console.log(getDb);
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  save() {
+    const db = getDb();
+    console.log(db, "db");
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then((res) => console.log("res@model", res))
+      .catch((err) => console.log(err));
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => console.log(err));
+  }
+}
+
+// const Product = sequelize.define("product", {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true,
+//   },
+//   title: Sequelize.STRING,
+//   price: {
+//     type: Sequelize.DOUBLE,
+//     allowNull: false,
+//   },
+//   imageUrl: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   description: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+// });
+
+module.exports = Product;
