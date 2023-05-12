62cd620f65c9e4e16ee81a55f25b614fb19e497f
Dhaval Thakkar <dhaval.t@simformsolutions.com>
2023-05-12 20:53:24 +0530

    added auth module 15: to start with module 15

---
+++ Udemy/Module-15-Authentication/controllers/auth.js
@@ -0,0 +1,115 @@
+const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const User = require("../models/user");
+
+const client = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "abcxyz121991@gmail.com",
+    pass: "qmypkvqyhvxminem",
+  },
+});
+exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    errorMessage: message,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    errorMessage: message,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "Invalid email or password");
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((isPassword) => {
+          console.log("wrong password");
+          if (isPassword) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("login");
+        });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email: email })
+    .then((foundUser) => {
+      if (foundUser) {
+        req.flash(
+          "error",
+          "Email exists already, please pick a different one."
+        );
+        return res.redirect("/signup");
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+          return client.sendMail({
+            from: "node-course@gmail.com",
+            to: email,
+            subject: "Signed up successfully",
+            html: "<h1>You have signed up successfully</h1>",
+            // text: "Hey, I'm being sent from the cloud",
+          });
+        });
+    })
+
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
